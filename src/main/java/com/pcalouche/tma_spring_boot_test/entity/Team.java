c4311cb53c21f31f12785489e5ed683866191baf
Deric <derickdd716@gmail.com>
2021-07-01 21:14:10 -0400

    Adds the remaining fields for the team entity

--- src/main/java/com/pcalouche/tma_spring_boot_test/entity/Team.java
+++ src/main/java/com/pcalouche/tma_spring_boot_test/entity/Team.java
@@ -8,6 +8,33 @@ public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String name;
+    private String city;
+    private String mascot;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getMascot() {
+        return mascot;
+    }
+
+    public void setMascot(String mascot) {
+        this.mascot = mascot;
+    }
 
     public Integer getId() {
         return id;
